ddb7928a422ead0f3e6c27ef13f91a4bdf9d3b10
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-05-17 14:03:37 -0700

    Revised output to be more colorful.

--- hello_world.ps1
+++ hello_world.ps1
@@ -1,3 +1,20 @@
-﻿#!/bin/powershell
+﻿# hello world in PowerShell
 
-Write-Host 'Hello World'
+# prompt user input
+Write-Host "`nHello World Exercise in PowerShell."
+Pause
+
+# define HelloWorld function
+function HelloWorld()
+{
+    Write-Host -ForegroundColor Red "`nHello, World!"
+    Start-Sleep -s 2
+    Write-Host -ForegroundColor Yellow "`nHello, World!"
+    Start-Sleep -s 2
+    Write-Host -ForegroundColor Green "`nHello, World!"
+    Start-Sleep -s 2
+    Write-Host -ForegroundColor Blue "`nHello, World!`n"
+}
+
+# call HelloWorld function 
+HelloWorld
