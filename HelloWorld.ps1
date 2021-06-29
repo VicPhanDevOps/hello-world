3de355526465936a0d9c69371b676a218792241d
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-29 07:14:31 -0700

    Added colors list, more colors, foreach loop, try, catch, logging and renamed script.

---
+++ HelloWorld.ps1
@@ -0,0 +1,24 @@
+﻿# hello world exercise in PowerShell
+
+function HelloWorld()
+{
+    Write-Host "`nHello World Exercise in PowerShell.`n"
+
+    $colors = 'White', 'DarkRed', 'Red', 'Yellow', 'DarkYellow', 'Green', 'DarkGreen', 'DarkBlue', 'Blue', 'DarkCyan', 'Cyan', 'Magenta', 'DarkMagenta', 'Black'
+
+    try 
+    {
+        foreach ($color in $colors)
+        {
+            Write-Host -ForegroundColor $color "Hello, World!"
+            Start-Sleep -s 1
+        }
+        Write-Host "`nSuccessfully ran Hello World exercise!`n" -ForegroundColor Green
+    }
+    catch 
+    {
+        Write-Host "`nFailed to run Hello World exercise.`n" -ForegroundColor Red
+    }
+}
+
+HelloWorld
